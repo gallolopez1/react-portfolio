c2047d0a406abb8ec9ea1b6ad4a7df4fd45697c2
Rooster <gallolopez1@hotmail.com>
2022-03-15 18:28:26 -0400

    Updated about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,26 +10,26 @@ function About(props) {
             </h2>
             <br />
             <div className='m-3' data-aos="fade-right" data-aos-duration="750">
-                <h4 className='text-wrap text-justify'>
+                <h4 className='text-wrap text-justify fw-lighter'>
                     <b className="curious">Dog lover</b>, <b className="curious">bibliophile</b> and <b className="curious">gym
                         enthusiast</b>. Originally from the beautiful and vibrant city of Guadalajara, Mexico, where I studied <b className="curious">architecture</b> and graduated from <a className="hyperlink" style={{ textDecoration: "underline", color: "#78ce92", fontWeight: "bold" }} href="https://www.tec.mx/en">Tec de Monterrey.</a>
                 </h4>
                 <br />
-                <h4 className='text-wrap text-justify'>
+                <h4 className='text-wrap text-justify fw-lighter'>
                     I then started working as a freelancer, developing architectural concepts, schematic designs and a few websites. In 2020 I moved to the craziest of cities <b className="curious">(New York City)</b>, where I currently live and work as an <b className="curious">architect designer</b> for <a className="hyperlink" style={{ textDecoration: "underline", color: "#78ce92", fontWeight: "bold" }} href="https://www.rockwellgroup.com/">Rockwell Group.</a>
                 </h4>
                 <br />
-                <h4 className='text-wrap text-justify'>
+                <h4 className='text-wrap text-justify fw-lighter'>
                     At <a className="hyperlink" style={{ textDecoration: "underline", color: "#78ce92", fontWeight: "bold" }} href="https://www.rockwellgroup.com/">Rockwell Group</a>, I have worked hand in hand with a diverse group of interior designers, architects and project managers on multiple projects ranging in scale, scope and location. From <b className="curious">educational architecture</b>, to <b className="curious">mixed-use commercial developments</b>, I have found that I become impassioned with whatever project I tackle.
                 </h4>
                 <br />
-                <h4 className='text-wrap'>
+                <h4 className='text-wrap text-justify fw-lighter'>
                     My expertise is in the fields of <b className="curious">architecture</b> and <b className="curious">design</b> with some exposure to <b className="curious">web development</b>, however, my passion and desire to constantly learn and evolve take me to explore new fields such as <b className="curious">finance</b> and <b className="curious">technology</b>. Recently earned a certificate in full-stack development from Columbia University with newly developed skills in HTML, CSS,  JavaScript, Node.js, MySQL, React.js, and responsive web design.
                 </h4>
                 <br></br>
                 <div className='text-center'>
                     <h4>
-                        Now that you know me, I would love to get to know you! Send me an <a style={{ textDecoration: "underline", textIndent: ".25em", color: "#e95262" }} href="mailto:gabrielgallo123@gmail.com"><b>email.</b></a>
+                        Now that you know me, I would love to get to know you! Send me an <a style={{ textDecoration: "underline", textIndent: ".24em", color: "#e95262" }} href="mailto:gabrielgallo123@gmail.com"><b>email.</b></a>
                     </h4>
                 </div>
             </div>
